8684018b3ab06bbab07ea41d9b2f704624850d0c
sergejnehorosev <sergo777ser777@gmail.com>
2024-12-03 12:12:11 +0300

    feat: add basic integration with sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 import React from "react";
 
 async function Home({
@@ -7,6 +8,22 @@ async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _id: 1,
+      _createdAt: new Date(),
+      views: 55,
+      description: "This is the description",
+      category: "Robots",
+      image:
+        "https://t3.ftcdn.net/jpg/05/59/87/12/240_F_559871209_pbXlOVArUal3mk6Ce60JuP13kmuIRCth.jpg",
+      title: "Mr. Robot",
+      author: {
+        _id: 1,
+        name: "Adrian",
+      },
+    },
+  ];
 
   return (
     <>
@@ -22,6 +39,22 @@ async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?.id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startup results</p>
+          )}
+        </ul>
       </section>
     </>
   );
